3d02bbd4b19d8e749c9922c1835aac858c671276
Piotr Nalepa <piotr.nalepa@ez.no>
2017-02-21 09:40:43 +0100

    Created base bundle structure

---
+++ bundle/DependencyInjection/EzSystemsMultiFileUploadExtension.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\MultiFileUploadBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class EzSystemsMultiFileUploadExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $config = [];
+        foreach ($configs as $subConfig) {
+            $config = array_merge_recursive($config, $subConfig);
+        }
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $this->prependYui($container);
+        $this->prependCss($container);
+    }
+
+    private function prependYui(ContainerBuilder $container)
+    {
+        # Directories where public resources are stored (relative to web/ directory).
+        $container->setParameter('multifile_upload.public_dir', 'bundles/ezsystemsmultifileupload');
+
+        $yuiConfigFile = __DIR__.'/../Resources/config/yui.yml';
+        $config = Yaml::parse(file_get_contents($yuiConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($yuiConfigFile));
+    }
+
+    private function prependCss(ContainerBuilder $container)
+    {
+        $cssConfigFile = __DIR__.'/../Resources/config/css.yml';
+        $config = Yaml::parse(file_get_contents($cssConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($cssConfigFile));
+    }
+}
